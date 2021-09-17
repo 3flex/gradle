3d541c5d74ace1eb048169ec817318948e9c8ee0
Bo Zhang <bo@gradle.com>
2021-09-17 14:12:45 +0800

    Fail compileAll

--- subprojects/core-api/src/main/java/org/gradle/api/Project.java
+++ subprojects/core-api/src/main/java/org/gradle/api/Project.java
@@ -56,8 +56,6 @@ import java.net.URI;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.LinkedHashSet;
-import java.util.concurrent.Callable;
 
 /**
  * <p>This interface is the main API you use to interact with Gradle from your build file. From a <code>Project</code>,
